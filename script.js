3127dd0e26dd789a139112db942f423c1b29d714
vennilapraveenkumar9-cyber <vennilapraveenkumar9@gmail.com>
2026-06-18 22:28:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-console.log("Joe Andrea Illam Website Loaded");
+// =====================================
+// JOE ANDREA ILLAM
+// SCRIPT.JS
+// =====================================
+
+// Navbar shadow on scroll
+
+window.addEventListener("scroll", () => {
+
+    const navbar = document.querySelector(".navbar");
+
+    if(window.scrollY > 50){
+
+        navbar.style.boxShadow =
+        "0 5px 25px rgba(0,229,255,0.15)";
+
+    }
+    else{
+
+        navbar.style.boxShadow =
+        "none";
+    }
+
+});
+
+
+// Smooth button animation
+
+const buttons =
+document.querySelectorAll(".hero-btn");
+
+buttons.forEach(button => {
+
+    button.addEventListener("mouseenter", () => {
+
+        button.style.transform =
+        "translateY(-6px) scale(1.03)";
+
+    });
+
+    button.addEventListener("mouseleave", () => {
+
+        button.style.transform =
+        "translateY(0px) scale(1)";
+    });
+
+});
+
+
+// Console message
+
+console.log(
+"Joe Andrea Illam Website Loaded Successfully"
+);
